a3cd1161321b63aaf17b9b19938b48176f0eeabc
martin.dvpj <martin.devora.pajares@gmail.com>
2021-07-23 17:54:40 +0100

    Preparing release 0.4.0-RC3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-version = "0.4.0-RC2"
+version = "0.4.0-RC3"
 setuptools.setup(
     name="lcbuilder", # Replace with your own username
     version=version,
