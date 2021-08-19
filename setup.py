2a895ba6a2524ccab4f473bbacafc60fb1a9bd65
martin.dvpj <martin.devora.pajares@gmail.com>
2021-08-19 23:38:16 +0100

    update foldedleastsquares version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
                         "photutils==1.0.2",
                         "scipy==1.5.4",
                         "tess-point==0.6.1",
-                        "foldedleastsquares==1.0.28",
+                        "foldedleastsquares==1.0.29",
                         "wotan==1.9"
     ]
 )
