c02fa5dc3d34b0401265a58141ea1825ded48cff
martin.dvpj <martin.devora.pajares@gmail.com>
2021-10-01 16:52:31 +0100

    removing string

--- lcbuilder/lcbuilder_class.py
+++ lcbuilder/lcbuilder_class.py
@@ -228,7 +228,7 @@ class LcBuilder:
                                                                          numpy.std(flux_corr), amplitude_threshold)
                 if remove_signal:
                     logging.info(
-                        "Reducing pulsation with period %sd, flux amplitude of %s, phase minima at %s and snr %s",
+                        "Reducing pulsation with period %sd, flux amplitude of %s, phase at %s and snr %s",
                         period, A, p, snr)
                     pulsations_df = pulsations_df.append(
                         {'period_s': period * 24 * 3600, 'frequency_microHz': frequency / 24 / 3600 * 1000000,
