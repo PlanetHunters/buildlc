b43e11d52acc940bf1f4cc904ed39e0d09b94cf1
martin.dvpj <martin.devora.pajares@gmail.com>
2022-01-28 11:22:33 +0000

    update lcbuilder

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
                         "scipy==1.5.4",
                         "tess-point==0.6.1",
                         'torch==1.9.0',
-                        "foldedleastsquares==1.0.31",
+                        "foldedleastsquares==1.0.32",
                         "wotan==1.9"
     ]
 )
