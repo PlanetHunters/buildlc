dc51a7697ddd46927aa1b4ab2e56ceb8f4f6eb86
martin.dvpj <martin.devora.pajares@gmail.com>
2022-10-13 10:34:40 +0100

    add eleanor requirement

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setuptools.setup(
     python_requires='>=3.8',
     install_requires=['numpy==1.22.3',
                         'astropy==5.0.4',
+                        'eleanor==2.0.5',
                         'pandas==1.3.1',
                         "lightkurve==2.2.0",
                         "photutils==1.0.2",
