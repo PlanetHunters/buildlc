de3f52a9c05685d472a5a6df274a4200790984e1
martin.dvpj <martin.devora.pajares@gmail.com>
2022-10-13 10:46:20 +0100

    add eleanor as submodule

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     python_requires='>=3.8',
     install_requires=['numpy==1.22.3',
                         'astropy==5.0.4',
-                        'eleanor==2.0.5',
+                        #'eleanor==2.0.5', included with submodule
                         'pandas==1.3.1',
                         "lightkurve==2.2.0",
                         "photutils==1.0.2",
