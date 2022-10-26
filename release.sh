483f5073457028c024cac3c7f40f80b8af527574
martin.dvpj <martin.devora.pajares@gmail.com>
2022-10-26 10:17:39 +0100

    release runs tests

--- release.sh
+++ release.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-git_tag=$1
-git pull
-sed -i '5s/.*/version = "'${git_tag}'"/' setup.py
-git add setup.py
-git commit -m "Preparing release ${git_tag}"
-git tag ${git_tag} -m "New release"
-git push && git push --tags
+rm tests.log
+rm dist* -r
+rm -r .tox
+rm -r .pytest_cache
+rm -r build
+set -e
+tox -r > tests.log
+tests_results=$(cat tests.log | grep "congratulations")
+if ! [[ -z ${tests_results} ]]; then
+  python3.8 -m venv lcbuilder-reqs
+  source lcbuilder-reqs/bin/activate
+  python3.8 -m pip install pip -U
+  python3.8 -m pip install numpy
+  python3.8 setup.py install
+  python3.8 -m pip list --format=freeze > requirements.txt
+  deactivate
+  git_tag=$1
+  git pull
+  sed -i '5s/.*/version = "'${git_tag}'"/' setup.py
+  git add requirements.txt
+  git add setup.py
+  git commit -m "Preparing release ${git_tag}"
+  git tag ${git_tag} -m "New release"
+  git push && git push --tags
