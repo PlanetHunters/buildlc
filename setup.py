7fc071c8f723ae875bd40e524dc7daf9601bbed1
martin.dvpj <martin.devora.pajares@gmail.com>
2023-11-15 08:10:42 +0000

    use only python3.10

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.10',
     install_requires=['numpy==1.23.5',
                         'astropy==5.0.4',
                         'everest-pipeline==2.0.12',
